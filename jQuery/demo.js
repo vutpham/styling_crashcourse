ee18807841699398de09aa4559b500a4054f1c33
Vu Pham <vutpham@outlook.com>
2017-08-07 10:57:01 -0700

    Add selector exercise

---
+++ jQuery/demo.js
@@ -0,0 +1,7 @@
+$("div").css("background-color", "purple");
+$(".highlight").css("width", "200px");
+$("#third").css("border", "2px orange dashed");
+
+let first = $("div")[0];
+
+$(first).css("color", "pink");
